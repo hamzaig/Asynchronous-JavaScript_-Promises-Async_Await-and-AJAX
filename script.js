39b1e3c286392ad91a476c07061defc2cf79eeea
hamzaig <hamzaig@yahoo.com>
2021-09-16 19:46:54 +0500

    AJAX Call_ XMLHttpRequest to get countries details

--- script.js
+++ script.js
@@ -5,12 +5,34 @@ const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
 
-const request = new XMLHttpRequest();
-request.open("GET", "https://restcountries.eu/rest/v2/name/pakistan");
-request.send();
 
-request.addEventListener("load", function () {
-    // const data = JSON.parse(this.responseText)[0];
-    const [data] = JSON.parse(this.responseText);
-    console.log(data);
-})
+const getCountryData = (country) => {
+
+    const request = new XMLHttpRequest();
+    request.open("GET", `https://restcountries.eu/rest/v2/name/${country}`);
+    request.send();
+
+    request.addEventListener("load", function () {
+        // const data = JSON.parse(this.responseText)[0];
+        const [data] = JSON.parse(this.responseText);
+        console.log(data);
+        const html = ` 
+        <article class="country">
+          <img class="country__img" src="${data.flag}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${((+data.population) / 1000000).toFixed(2)}M People</p>
+            <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+            <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+          </div>
+        </article>`;
+        countriesContainer.insertAdjacentHTML("beforeend", html);
+        countriesContainer.style.opacity = 1;
+
+    })
+};
+
+getCountryData("pakistan");
+getCountryData("usa");
+getCountryData("india");
